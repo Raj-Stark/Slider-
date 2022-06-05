78e56084174fddd3f7c97ca12f865d871baa9243
Raj-Stark <rpal778866@gmail.com>
2022-06-05 17:46:55 +0530

    Initial Push

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+export default App;
